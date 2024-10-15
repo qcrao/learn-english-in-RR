3b384599fa1dbf9d0fb4dee3b6da5db800ea1848
qcrao <qcrao91@gmail.com>
2024-10-15 17:43:26 +0800

    add model config

--- src/index.js
+++ src/index.js
@@ -1,5 +1,27 @@
-function onload({ extensionAPI, ...rest }) {
+export let defaultModel;
+
+async function onload({ extensionAPI, ...rest }) {
   console.log("Loaded learn english in roam");
+
+  const panelConfig = {
+    tabTitle: "Learn English in RR",
+    settings: [
+      {
+        id: "model-provider",
+        name: "Model Provider",
+        description: "Choose the model provider",
+        action: {
+          type: "select",
+          items: ["OpenAI", "GitHub"],
+          onChange: (value) => {
+            console.log(value);
+          },
+        },
+      },
+    ],
+  };
+
+  await extensionAPI.settings.panel.create(panelConfig);
 }
 
 function onunload() {
