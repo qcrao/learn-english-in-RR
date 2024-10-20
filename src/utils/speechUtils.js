0df8e3183892ee777d006604d21a511fabd76892
qcrao <qcrao91@gmail.com>
2024-10-20 10:52:04 +0800

    wip: fix voices

--- src/utils/speechUtils.js
+++ src/utils/speechUtils.js
@@ -1,28 +1,47 @@
 import { selectedVoiceName } from "../config";
 
-export const speakText = (text) => {
+// Define the speak function that accepts a voice object and text
+const speakWithVoice = (voice, text) => {
   const utterance = new SpeechSynthesisUtterance(text);
+  utterance.voice = voice;
   utterance.rate = 0.8;
 
-  // Get available voices
-  const voices = speechSynthesis.getVoices();
-
-  // if selectedVoiceName is not found, use Nicky
-  // if (!selectedVoiceName) {
-  //   selectedVoiceName = "Nicky";
-  // }
-
-  // Select the specified voice
-  let selectedVoice = voices.find((voice) => voice.name === selectedVoiceName);
-
-  // If the specified voice is found, use it
-  if (!selectedVoice) {
-    console.warn("Selected voice not found, using default voice");
-    selectedVoice = voices[0]; // Use the first available voice as default
-  }
-
-  utterance.voice = selectedVoice;
+  console.log(
+    `Voice: ${voice.name}, URI: ${voice.voiceURI}, Language: ${voice.lang}`
+  );
 
   speechSynthesis.cancel();
   speechSynthesis.speak(utterance);
+};
+
+export const speakText = (text) => {
+  // Listen for voices to be loaded
+  speechSynthesis.onvoiceschanged = () => {
+    const voices = speechSynthesis.getVoices();
+    let selectedVoice = voices.find(
+      (voice) => voice.name === selectedVoiceName
+    );
+
+    if (!selectedVoice) {
+      console.warn("Selected voice not found, using default voice");
+      selectedVoice = voices[0]; // Use the first available voice as default
+    }
+
+    speakWithVoice(selectedVoice, text);
+  };
+
+  // Manually trigger voice loading check (needed for some browsers)
+  if (speechSynthesis.getVoices().length !== 0) {
+    const voices = speechSynthesis.getVoices();
+    let selectedVoice = voices.find(
+      (voice) => voice.name === selectedVoiceName
+    );
+
+    if (!selectedVoice) {
+      console.warn("Selected voice not found, using default voice");
+      selectedVoice = voices[0]; // Use the first available voice as default
+    }
+
+    speakWithVoice(selectedVoice, text);
+  }
 };
