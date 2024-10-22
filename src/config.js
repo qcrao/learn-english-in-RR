1516861eb9745f5e9098a1e3872028b77eb53b02
qcrao <qcrao91@gmail.com>
2024-10-22 10:30:47 +0800

    adjust the order of config

--- src/config.js
+++ src/config.js
@@ -13,7 +13,7 @@ export function loadInitialSettings(extensionAPI) {
   OPENAI_API_KEY = extensionAPI.settings.get("openai-api-key");
   openaiLibrary = initializeOpenAIAPI(OPENAI_API_KEY);
 
-  defaultOpenAIModel = extensionAPI.settings.get("model-provider");
+  defaultOpenAIModel = extensionAPI.settings.get("openai-model");
   if (!defaultOpenAIModel) defaultOpenAIModel = "GPT-4o-mini";
 
   streamResponse = extensionAPI.settings.get("streamResponse");
@@ -28,56 +28,18 @@ export function initPanelConfig(extensionAPI) {
     tabTitle: "Learn English in RR",
     settings: [
       {
-        id: "model-provider",
-        name: "Model Provider",
-        description: "Choose the model provider",
+        id: "voice-selection",
+        name: "Voice",
+        description: "Select the preferred voice",
         action: {
           type: "select",
-          items: [
-            "GPT-4o-mini",
-            "GPT-4o",
-            "GPT-4 Turbo",
-            "GPT-4",
-            "GPT-3.5-turbo",
-            "o1-preview",
-            "o1-mini",
-          ],
+          items: ["Nicky", "Aaron", "Daniel", "Junior"],
           onChange: (value) => {
-            defaultOpenAIModel = value;
-          },
-        },
-      },
-      {
-        id: "streamResponse",
-        name: "Stream response",
-        description:
-          "Stream responses of GPT models and OpenRouter streamable models:",
-        action: {
-          type: "switch",
-          onChange: (evt) => {
-            streamResponse = !streamResponse;
-          },
-        },
-      },
-      {
-        id: "openai-api-key",
-        name: "OpenAI API Key (GPT)",
-        description: (
-          <>
-            <span>Copy here your OpenAI API key for Whisper & GPT models</span>
-            <br></br>
-            <a href="https://platform.openai.com/api-keys" target="_blank">
-              (Follow this link to generate a new one)
-            </a>
-          </>
-        ),
-        action: {
-          type: "input",
-          onChange: (evt) => {
-            setTimeout(() => {
-              OPENAI_API_KEY = evt.target.value;
-              openaiLibrary = initializeOpenAIAPI(OPENAI_API_KEY);
-            }, 200);
+            if (value === "Daniel") {
+              selectedVoiceName = "Daniel (English (United Kingdom))";
+            } else {
+              selectedVoiceName = value;
+            }
           },
         },
       },
@@ -95,7 +57,8 @@ export function initPanelConfig(extensionAPI) {
             e.g.: zh, en, es, fr...{" "}
             <a
               href="https://en.wikipedia.org/wiki/List_of_ISO_639-1_codes"
-              target="_blank">
+              target="_blank"
+            >
               (See ISO 639-1 codes here)
             </a>
           </>
@@ -108,18 +71,56 @@ export function initPanelConfig(extensionAPI) {
         },
       },
       {
-        id: "voice-selection",
-        name: "Voice",
-        description: "Select the preferred voice",
+        id: "streamResponse",
+        name: "Stream response",
+        description:
+          "Stream responses of GPT models and OpenRouter streamable models:",
+        action: {
+          type: "switch",
+          onChange: (evt) => {
+            streamResponse = !streamResponse;
+          },
+        },
+      },
+      {
+        id: "openai-model",
+        name: "OpenAI Model",
+        description: "Choose the OpenAI model",
         action: {
           type: "select",
-          items: ["Nicky", "Aaron", "Daniel", "Junior"],
+          items: [
+            "GPT-4o-mini",
+            "GPT-4o",
+            "GPT-4 Turbo",
+            "GPT-4",
+            "GPT-3.5-turbo",
+            "o1-preview",
+            "o1-mini",
+          ],
           onChange: (value) => {
-            if (value === "Daniel") {
-              selectedVoiceName = "Daniel (English (United Kingdom))";
-            } else {
-              selectedVoiceName = value;
-            }
+            defaultOpenAIModel = value;
+          },
+        },
+      },
+      {
+        id: "openai-api-key",
+        name: "OpenAI API Key (GPT)",
+        description: (
+          <>
+            <span>Copy here your OpenAI API key for GPT models</span>
+            <br></br>
+            <a href="https://platform.openai.com/api-keys" target="_blank">
+              (Follow this link to generate a new one)
+            </a>
+          </>
+        ),
+        action: {
+          type: "input",
+          onChange: (evt) => {
+            setTimeout(() => {
+              OPENAI_API_KEY = evt.target.value;
+              openaiLibrary = initializeOpenAIAPI(OPENAI_API_KEY);
+            }, 200);
           },
         },
       },
